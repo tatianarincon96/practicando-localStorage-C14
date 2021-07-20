581ce88869255790953cfd6b7454b811fec764bd
tatianarincon96 <tatianalunarincon@gmail.com>
2021-07-20 16:48:03 -0300

    [ADD] activity local storage

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ REQUERIMIENTOS
 
 - constantemente guardar la informacion en localStorage, si se recarga la pagina deberian mantenerse los comentarios
 */
+
+window.onload = () => {
+    const form = document.forms[0];
+    const comentario = form.comentario;
+    const arrayComentarios = [];
+
+    form.onsubmit = (evento) => {
+        evento.preventDefault();
+        arrayComentarios.push(comentario.value);
+        localStorage.setItem("comentario", JSON.stringify(arrayComentarios));
+        renderizarComentario(comentario.value);
+    }
+}
+
+
+function renderizarComentario(comentario) {
+    const contenedor = document.querySelector("#comentarios");
+    const template = `<p>${comentario}</p>`;
+    contenedor.innerHTML += template;
+}
